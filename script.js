7eb89fe6acdc0afe435dd67ca878aebbe3fd91e0
Fayoung01 <fayounguhie@gmail.com>
2020-04-07 17:13:17 +0100

    Get error input field first letter as uppercased

--- script.js
+++ script.js
@@ -25,11 +25,16 @@ function isValidEmail(email) {
 function checkRequired(inputArr) {
   inputArr.forEach(function (input) {
     if (input.value.trim() === "") {
-      showError(input, `${input.id} is required`);
+      showError(input, `${getFieldName(input)} is required`);
     } else {
       showSuccess(input);
     }
   });
+}
+
+// Get error input fields first letter as uppercase
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // Event Listeners
